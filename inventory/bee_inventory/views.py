44540a586b3ae0198e1aabbea602c76c65837f0b
virginijus10 <virgijijus.baranauskas@yahoo.com>
2024-03-28 12:44:11 +0200

    vartotoju vaizdas

--- inventory/bee_inventory/views.py
+++ inventory/bee_inventory/views.py
@@ -21,55 +21,12 @@ class About_us(TemplateView):
 	template_name = 'bee_inventory/about_us.html'
 
 
-# class Dashboard(LoginRequiredMixin, View):
-# 	def get(self, request):
-# 		items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
-
-# 		low_inventory = InventoryItem.objects.filter(
-# 			#user=self.request.user.id,
-# 			quantity__lte=LOW_QUANTITY
-# 		)
-
-# 		if low_inventory.count() > 0:
-# 			if low_inventory.count() > 1:
-# 				messages.error(request, f'{low_inventory.count()} items have low inventory')
-# 			else:
-# 				messages.error(request, f'{low_inventory.count()} item has low inventory')
-
-# 		low_inventory_ids = InventoryItem.objects.filter(
-# 			user=self.request.user.id,
-# 			quantity__lte=LOW_QUANTITY
-# 		).values_list('id', flat=True)
-
-# 		return render(request, 'bee_inventory/dashboard.html', {'items': items, 'low_inventory_ids': low_inventory_ids})
-
-# class Dashboard(LoginRequiredMixin, View):
-#     def get(self, request):
-#         # Gauti visus inventoriaus elementus
-#         items = InventoryItem.objects.all().order_by('id')
-
-#         # Gauti inventoriaus elementus, kurių kiekis yra mažesnis arba lygus nustatytam mažiausiam kiekiui
-#         low_inventory = InventoryItem.objects.filter(
-#             quantity__lte=LOW_QUANTITY
-#         )
-
-#         if low_inventory.exists():
-#             messages.error(request, f'Yra prekių, kurių kiekis yra mažesnis nei nustatyta riba')
-
-#         low_inventory_ids = low_inventory.values_list('id', flat=True)
-
-#         return render(request, 'bee_inventory/dashboard.html', {'items': items, 'low_inventory_ids': low_inventory_ids})
-
-
-
-
-class Dashboard(UserPassesTestMixin, View):
-    def test_func(self):
-        return self.request.user.is_authenticated and self.request.user.is_staff
-
+class Dashboard(LoginRequiredMixin, View):
     def get(self, request):
+        # Gauti visus inventoriaus elementus
         items = InventoryItem.objects.all().order_by('id')
 
+        # Gauti inventoriaus elementus, kurių kiekis yra mažesnis arba lygus nustatytam mažiausiam kiekiui
         low_inventory = InventoryItem.objects.filter(
             quantity__lte=LOW_QUANTITY
         )
